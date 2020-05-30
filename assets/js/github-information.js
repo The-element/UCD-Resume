834a2d67638b1af48ec3c99d169cc46d1183bc99
The-element <nicke.jonsson@live.com>
2020-05-30 10:44:04 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -22,6 +22,27 @@ function userInformationHTML(user) {    //creates a function by the name of "use
         //if the profile pic cant be displayed, the placeholder in the alt-attribute will make sure the username is displayed instead.
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {    //checks to see if there is any repos in the response, if not do the following:
+        return `<div class="clearfix repo-list">No repos!</div>`; //return this message.
+    }
+
+    var listItemsHTML = repos.map(function(repo) {  //creates the variable listItemsHTML to store the return from the function "repos.map", which in it's turn is created when the function inside it is run, a function in which we also pass in the object repo.
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`; //Lists the repo-objects html-url and its name.
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`; //joins the list items into an unordered list. Separated by a new line for each item.
+}
+
 function fetchGitHubInformation(event) {    //Is being called when writing in the input-text-form.
 
     var username = $("#gh-username").val(); //stores the username input by calling the jquery-funtion value(val()).
@@ -35,12 +56,15 @@ function fetchGitHubInformation(event) {    //Is being called when writing in th
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
     
-    $.when( //When the following code has run,
-        $.getJSON(`https://api.github.com/users/${username}`) //using Jquery getJSON to retrive the data, we pars in the url + value of username from the variable above with the same name.
+    $.when( //When the following code has run. when we do two calls like this, the when() method packs a response up into arrays.
+        $.getJSON(`https://api.github.com/users/${username}`), //using Jquery getJSON to retrive the data, we pars in the url + value of username from the variable above with the same name.
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then( //Then do the following:
-        function(response) {    //if we get a respons, pass in the response to this function, and do the following:
-            var userData = response; //store the response in the variable userData.
+        function(firstResponse, secondResponse) {    //if we get a respons, pass in each response to this function, and do the following:
+            var userData = firstResponse[0];; //store the first response from the array-variable userData at index 0.
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData)); //using Jquery to target the div-element with the id of "gh-user-data", we set its content to html and pass in the function userInformationHTML(), together with the response.
+            $("#gh-repo-data").html(repoInformationHTML(repoData)); //using Jquery to target the div-element with the id of "gh-user-data", we set its content to html and pass in the function repoInformationHTML(), together with the response.
         },
         function(errorResponse) {   //if we get a errorResponse, do the following:
             if (errorResponse.status === 404) { //if the status of the errorResponse is equal to 404, do the following:
